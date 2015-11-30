4a4bd9fe96a02a9f561d1669c0505e7d265fcbd8
BradenHowell <bxh140330@utdallas.edu>
2015-11-29 19:37:44 -0600

    Update Letter_Matrix_Generator.asm

--- src/asm/Letter_Matrix_Generator.asm
+++ src/asm/Letter_Matrix_Generator.asm
@@ -1,7 +1,9 @@
 	.data
-	
+state_board:	.bytes 0, 0, 0, 0, 0, 0, 0, 0, 0		#letter matrix	
+
+#start copying here
+	.data
 Map:	.space 26		#hash map
-state_board:	.bytes 0, 0, 0, 0, 0, 0, 0, 0, 0		#letter matrix
 
 	.text
 GEN_MATRIX:
@@ -10,7 +12,7 @@ GEN_MATRIX:
 	
 	li $t0, 0
 	la $t1, state_board
-FOR_GEN:				#get 9 letters
+GEN_MATRIX_FOR:				#get 9 letters
 	jal RNGALPHA		#get random letter
 	
 	add $a0, $v0, $zero
@@ -20,7 +22,7 @@ FOR_GEN:				#get 9 letters
 	sb $v0, ($t2)
 	
 	addi $t0, $t0, 4	#loop control
-	bne $t0, 36, FOR_GEN
+	bne $t0, 36, GEN_MATRIX_FOR
 #End for loop
 			
 	lw $ra, ($sp)		#restore stack
@@ -31,36 +33,36 @@ FOR_GEN:				#get 9 letters
 
 
 
-RNGALPHA:
+GEN_MATRIX_RNG:
 	li $v0, 41		#get random number
 	syscall
 	srl $a0, $a0, 26	#divide to within 0-31
 	
 	slti $t2, $a0, 26	#if the result is greater than 25, try again
-	beq $t2 $zero, RNGALPHA
+	beq $t2 $zero, GEN_MATRIX_RNG
 	
 	add $v0, $a0, $zero	#put random number in $v0
 	jr $ra
 	
 	
 	
-HASHALPHA:
+GEN_MATRIX_HASH:
 	la $t2, Map
-WHILE_HASH:
+GEN_MATRIX_WHILE:
 	add $t3, $a0, $zero
 	add $t3, $t3, $t2	#get current byte
 	
 	lb $t4, ($t3)
-	beq $t4, $zero, VALID_HASH	#check if byte is used
+	beq $t4, $zero, GEN_MATRIX_VALID	#check if byte is used
 	
-	beq $a0, 26, WRAP	#check if needs to wrap back to 0
+	beq $a0, 26, GEN_MATRIX_WRAP	#check if needs to wrap back to 0
 	addi $a0, $a0, 1
-	j NO_WRAP
-WRAP:
+	j GEN_MATRIX_NO_WRAP
+GEN_MATRIX_WRAP:
 	sub $a0, $a0, 26
-NO_WRAP:	
-	j WHILE_HASH
-VALID_HASH:	
+GEN_MATRIX_NO_WRAP:	
+	j GEN_MATRIX_WHILE
+GEN_MATRIX_VALID:	
 	li $t4, 1
 	sb $t4, ($t3)		#mark letter as used
 	add $v0, $a0, $zero	#final letter
